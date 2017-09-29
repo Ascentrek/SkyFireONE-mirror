ff578da37ede1b31e3a3713c80e1455d06db74ef
Bootz <bootz@projectskyfire.org>
2017-09-28 21:19:28 -0500

    Core/Scripts: Cleaned/Updated last 2 OPvP_scripts Zangarmarsh(ZM) & Terokkar Forest(TF)
    Note: Keep in mind these are just initial scripting...
    cleaned/updated, just means fit to our ScriptSystem...
    Patience and they will get finished.
    
    Signed-off-by: Bootz <bootz@projectskyfire.org>

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -415,8 +415,8 @@ void AddSC_outdoorpvp_ep();
 void AddSC_outdoorpvp_hp();
 void AddSC_outdoorpvp_na();
 void AddSC_outdoorpvp_si();
-//void AddSC_outdoorpvp_tf();
-//void AddSC_outdoorpvp_zm();
+void AddSC_outdoorpvp_tf();
+void AddSC_outdoorpvp_zm();
 
 // player
 //void AddSC_chat_log();
@@ -853,8 +853,8 @@ void AddOutdoorPvPScripts()
     AddSC_outdoorpvp_hp();
     AddSC_outdoorpvp_na();
     AddSC_outdoorpvp_si();
-    //AddSC_outdoorpvp_tf();
-    //AddSC_outdoorpvp_zm();
+    AddSC_outdoorpvp_tf();
+    AddSC_outdoorpvp_zm();
 #endif
 }
 
